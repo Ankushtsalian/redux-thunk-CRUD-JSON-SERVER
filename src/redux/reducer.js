42daf9ea04c6fe81c724f9e57736ca0619089887
Ankushtsalian <ankushtsalian@gmail.com>
2022-06-08 14:06:21 +0530

    added INIT REDUCER

---
+++ src/redux/reducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+  users: [],
+  user: {},
+  loading: false,
+};
+
+const userReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case type:
+      return;
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
