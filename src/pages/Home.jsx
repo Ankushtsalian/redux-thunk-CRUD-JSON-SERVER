447f7e5ea6db370b69842422eafb34374b618670
Ankushtsalian <ankushtsalian@gmail.com>
2022-06-08 18:18:38 +0530

    updated store

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,9 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 // import Paper from "@mui/material/Paper";
+import { useSelwctor, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { loadUsers } from "../redux/actions";
 
 //MUI COMP>>CUSTOMIZATION>>Customization
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -33,15 +36,22 @@ function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+// const rows = [
+//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+//   createData("Eclair", 262, 16.0, 24, 6.0),
+//   createData("Cupcake", 305, 3.7, 67, 4.3),
+//   createData("Gingerbread", 356, 16.0, 49, 3.9),
+// ];
 
 const Home = () => {
+  // const {}
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUsers);
+  }, []);
+
   return (
     <div>
       <TableContainer
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,21 @@ import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
 import rootReducer from "./root-reducer";
 
-const middlewares = [reduxThunk];
+// const middlewares = [reduxThunk];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
+const middlewares = [
+  process.env.NODE_ENV !== "production" && reduxThunk,
+].filter(Boolean);
+
+const composeEnhancer =
+  process.env.NODE_ENV !== "production" &&
+  window &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
+// export const store = createStore(rootReducer, undefined, composedEnhancers);
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
 export default store;
