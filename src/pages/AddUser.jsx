e46b3d13d057430090f1cd6d4ff84f011d3e587b
Ankushtsalian <ankushtsalian@gmail.com>
2022-06-11 20:14:34 +0530

    created AddUser form

--- src/pages/AddUser.jsx
+++ src/pages/AddUser.jsx
@@ -1,7 +1,34 @@
+import { Box, TextField } from "@mui/material";
 import React from "react";
+import { useState } from "react";
 
 const AddUser = () => {
-  return <div>AddUser</div>;
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    contact: "",
+    address: "",
+  });
+  const { name, email, contact, address } = state;
+  return (
+    <div>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "45ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <TextField id="standard-basic" label="standard" variant="standard" />
+          <TextField id="standard-basic" label="standard" variant="standard" />
+          <TextField id="standard-basic" label="standard" variant="standard" />
+          <TextField id="standard-basic" label="standard" variant="standard" />
+        </div>
+      </Box>
+    </div>
+  );
 };
 
 export default AddUser;
