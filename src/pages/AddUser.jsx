5066d0a5fa26df0a8f0bcfc48c9457d4a40ba96f
Ankushtsalian <ankushtsalian@gmail.com>
2022-06-11 20:43:57 +0530

    created AddUser form with states

--- src/pages/AddUser.jsx
+++ src/pages/AddUser.jsx
@@ -1,6 +1,7 @@
 import { Box, Button, TextField } from "@mui/material";
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddUser = () => {
   const [state, setState] = useState({
@@ -10,11 +11,29 @@ const AddUser = () => {
     address: "",
   });
   const { name, email, contact, address } = state;
+
+  const handleInput = (event) => {
+    const { name, value } = event.target;
+    console.log(state);
+    setState((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const Navigate = useNavigate();
   return (
     <div>
+      <Button
+        style={{ marginTop: "20px" }}
+        color="secondary"
+        variant="contained"
+        onClick={() => Navigate("/")}
+      >
+        Go Back
+      </Button>
+      <h2>Add User</h2>
       <Box
         component="form"
         sx={{
+          marginTop: "100px",
           "& .MuiTextField-root": { m: 1, width: "45ch" },
         }}
         noValidate
@@ -22,35 +41,43 @@ const AddUser = () => {
       >
         <div>
           <TextField
-            id="standard-basic"
+            id="name"
             label="Name"
+            name="name"
             variant="standard"
             value={name}
             type="text"
+            onChange={handleInput}
           />
           <br />
           <TextField
-            id="standard-basic"
+            id="Email"
             label="Email"
             variant="standard"
             value={email}
             type="email"
+            name="email"
+            onChange={handleInput}
           />
           <br />
           <TextField
-            id="standard-basic"
+            id="Contact"
             label="Contact"
             variant="standard"
             value={contact}
+            name="contact"
             type="number"
+            onChange={handleInput}
           />
           <br />
           <TextField
-            id="standard-basic"
+            id="Address"
             label="Address"
             variant="standard"
             value={address}
+            name="address"
             type="text"
+            onChange={handleInput}
           />
           <br />
           <Button
